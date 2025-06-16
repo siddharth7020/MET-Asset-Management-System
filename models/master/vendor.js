a2b4aef136894018dfe3ea9f4fbf829cc33438b4
siddharth7020 <sidh1583@gmail.com>
2025-06-16 10:10:38 +0530

    Remove validation rules from Vendor model fields for simplified data handling

--- models/master/vendor.js
+++ models/master/vendor.js
@@ -10,52 +10,30 @@ const Vendor = sequelize.define('vendor', {
     name: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'Name is required' },
-            len: { args: [3, 50], msg: 'Name must be between 3 to 50 characters' }
-        }
+       
     },
     companyName: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'Company Name is required' }
-        }
+      
     },
     address: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'Address is required' }
-        }
+     
     },
     email: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'Email is required' },
-            isEmail: { msg: 'Invalid email format' }
-        }
+       
     },
     mobileNo: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'Mobile number is required' },
-            isNumeric: { msg: 'Mobile number must contain only digits' },
-            len: { args: [10, 10], msg: 'Mobile number must be 10 digits' }
-        }
     },
     pancardNo: {
         type: Sequelize.STRING,
         allowNull: false,
-        validate: {
-            notEmpty: { msg: 'PAN Card number is required' },
-            is: {
-                args: /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/,
-                msg: 'Invalid PAN Card number format'
-            }
-        }
     },
     gstNo: {
         type: Sequelize.STRING,
